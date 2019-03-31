606fada09ec37e2fbf8be5d5cd9c6fa408b9c8b2
wu rengui <rengui.wu@advance.ai>
2019-03-31 21:46:27 +0800

    photo

--- nuxt-blog/components/photo-item.vue
+++ nuxt-blog/components/photo-item.vue
@@ -2,7 +2,7 @@
   <div>
     <div v-for='(item1,index1) in imgsData' :key='index1' class="section">
       <div class="date">
-        {{item1.data}}
+        {{item1.date}}
       </div>
       <div v-for="(item2,key2) in item1.imgs" :key='key2' class="imgs">
         <div class="img">
@@ -16,13 +16,42 @@
 export default {
   data() {
     return {
-
+      // imgsData: [
+      //   {
+      //     date: '2019-03-01',
+      //     imgs: {
+      //       imgs: ['https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1554048776727&di=c4f7b4ab773adbf89b4c6fcf1d873df9&imgtype=0&src=http%3A%2F%2Fwww.goupuzi.com%2Fnewatt%2FMon_1810%2F1_177922_7b827fc5298cf03.jpg']
+      //     }
+      //   }
+        
+      // ]
+      
     }
+  },
+  created() {
+    console.log(this.imgsData)
   },
   props: {
     imgsData: {
       type: Array,
-      default: []
+      default: [
+        {
+          date: '2019-03-01',
+          imgs: [
+            {
+              src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1554048776727&di=c4f7b4ab773adbf89b4c6fcf1d873df9&imgtype=0&src=http%3A%2F%2Fwww.goupuzi.com%2Fnewatt%2FMon_1810%2F1_177922_7b827fc5298cf03.jpg'
+            }
+          ]
+        },
+        {
+          date: '2019-03-01',
+          imgs: [
+            {
+              src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1554048776727&di=c4f7b4ab773adbf89b4c6fcf1d873df9&imgtype=0&src=http%3A%2F%2Fwww.goupuzi.com%2Fnewatt%2FMon_1810%2F1_177922_7b827fc5298cf03.jpg'
+            }
+          ]
+        },
+      ]
     }
   }
 }
